998ebdf465e6175a75694cf55aaab6cba3975c1b
okcompiler <139511195+okcompiler@users.noreply.github.com>
2024-07-17 15:30:06 -0500

    Add ability to save RSS feed posts to database

--- handler_user.go
+++ handler_user.go
@@ -40,3 +40,20 @@ func (ac *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 func (ac *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
+
+func (ac *apiConfig) handlerGetPostsForUser(
+	w http.ResponseWriter,
+	r *http.Request,
+	user database.User,
+) {
+	posts, err := ac.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  10,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get posts: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databasePostsToPosts(posts))
+}
